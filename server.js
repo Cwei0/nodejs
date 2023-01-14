3a4711bada11933c47a71cfdeb9c7ba781547fdc
Cwei0 <bottgabriel@icloud.com>
2023-01-14 19:32:56 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+console.log("Starting")
+
+// console.log(global)
+//  module importing with Common JS module and es6 module
+
+// import os from 'os' 
+const os = require('os');
+const path = require('path') //Common Core module import
+const {add, divide} = require('./math') //Custom module & destructuring
+
+console.log(divide(5,8))
+
+// console.log(os.type())
+// console.log(os.version())
+// console.log(os.platform())
+// console.log(os.arch())
+// console.log(os.homedir())
+
+// console.log(__dirname)
+// console.log(__filename)
+
+// console.log(path.dirname(__filename))
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename))
+// console.log(path.parse(__filename))
+
