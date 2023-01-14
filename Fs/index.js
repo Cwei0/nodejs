acd9ba0ab20845911601f5ce9e826e5476c2a99c
Cwei0 <bottgabriel@icloud.com>
2023-01-14 23:25:18 +0100

    ex2

--- Fs/index.js
+++ Fs/index.js
@@ -9,15 +9,23 @@ fs.readFile(path.join(__dirname,'./files', 'starter.txt'), 'utf8', (err, data) =
 //toString() => 'utf8'
 console.log('Hello...')
 
+//it is wise to append the text with the write function
+
 fs.writeFile(path.join(__dirname,'./files', 'reply.txt'),'Nice to meet you.', (err) => {
     if (err) throw err
     console.log('Write Complete')
+
+    fs.appendFile(path.join(__dirname, './files', 'reply.txt'), '\n\n Yes it is', (err) => {
+        if (err) throw err
+        console.log('Append Complete')
+
+        fs.rename(path.join(__dirname, './files', 'reply.txt'), path.join(__dirname, './files', 'newReply.txt'), (err) => {
+            if (err) throw err
+            console.log('Renamed Complete')
+        })
+    })
 })
 
-fs.appendFile(path.join(__dirname, './files', 'testing.txt'), 'Testing text', (err) => {
-    if (err) throw err
-    console.log('Append Complete')
-})
 
 
 //uncaught errors
