5c2d3baf617430e4a353847b3da6e72e9b1d029f
Cwei0 <bottgabriel@icloud.com>
2023-01-17 23:08:32 +0100

    final

--- server.js
+++ server.js
@@ -1,110 +1,64 @@
-// console.log("Starting")
-const http = require("http");
+const express = require('express')
 
-const fs = require("fs");
-
-const fsPromises = require("fs").promises;
-
+const app = express()
 const path = require("path");
 
-const logEvents = require('./logEvent')
+const PORT = process.env.PORT || 3500
 
-const EventEmitter = require('events')
-
-class MyEmitter extends EventEmitter { }
-
-//initialize object
-const myEmitter = new MyEmitter()
-
-myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName))
-
-const HOST = 'localhost'
-
-const PORT = process.env.PORT || 4500
-
-const serverFile = async (filePath, contentType, res) => {
-    try {
-        const rawData = await fsPromises.readFile(
-            filePath,
-            !contentType.includes('image') ? 'utf8' : ''
-        )
-        const data = contentType === 'application/json' ? JSON.parse(rawData) : rawData
-        res.writeHead(
-            filePath.includes('404.html') ? 404 : 200, { 'Content-Type': contentType }
-        )
-        res.end(
-            contentType === 'application/json' ? JSON.stringify(data) : data
-        )
-    } catch (err) {
-        console.error(err)
-        myEmitter.emit('log', `${err.name}:${err.message}`, 'errLog.txt')
-        res.statusCode = 500
-        res.end()
-    }
-
-
-}
-
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method)
-    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt')
-
-
-    const extension = path.extname(req.url)
-
-    let contentType;
-
-    switch (extension) {
-        case '.css':
-            contentType = 'text/css'
-            break
-        case '.js':
-            contentType = 'text/javascript'
-            break
-        case '.json':
-            contentType = 'application/json'
-            break
-        case '.jpg':
-            contentType = 'image/jpeg'
-            break
-        case '.png':
-            contentType = 'image/png'
-            break
-        case '.txt':
-            contentType = 'text/plain'
-            break
-        default:
-            contentType = 'text/html'
-    }
-
-    let filePath = contentType === 'text/html' && req.url === '/' ? path.join(__dirname, 'views', 'index.html') : contentType === 'text/html' && req.url.slice(-1) === '/' ? path.join(__dirname, 'views', req.url, 'index.html') : contentType === 'text/html' ? path.join(__dirname, 'views', req.url) : path.join(__dirname, req.url)
-
-    if (!extension && req.url.slice(-1) !== '/') filePath += 'html';
-
-    const fileExist = fs.existsSync(filePath);
-
-    if (fileExist) {
-        serverFile(filePath, contentType, res)
-    } else {
-        //301
-        //404
-        switch (path.parse(filePath).base) {
-            case 'old-page.html':
-                res.writeHead(301, { 'Location': '/new-page.html' })
-                res.end()
-                break
-
-            case 'www-page.html':
-                res.writeHead(301, { 'Location': '/' })
-                res.end()
-                break
-            default:
-                serverFile(path.join(__dirname, 'views', '404.html'), 'text/html', res)
-        }
-    }
+app.get('^/$|/index(.html)?', (req, res) => {
+    // res.sendFile('./views/index.html', {root: __dirname})
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
+app.get('/new-page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
+})
 
-server.listen(PORT, HOST, () => {
+app.get('/old-page(.html)?', (req, res) => {
+    res.redirect(301, '/new-page.html')
+})
+
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
+
+//Route handlers
+//Example 1
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('Attempted to load hello html page')
+    next()
+}, (req, res) => {
+    res.send('Hello World!')
+})
+//Example 2
+const cb0 = function (req, res, next) {
+    console.log('CB0')
+    next()
+}
+
+const cb1 = function (req, res, next) {
+    console.log('CB1')
+    next()
+}
+
+const cb2 = function (req, res) {
+    res.send('Hello from C!')
+}
+app.get('/example/c', [cb0, cb1, cb2])
+
+//App.route() fn used to chain route handlers
+app.route('/book')
+    .get((req, res) => {
+        res.send('Get a random book')
+    })
+    .post((req, res) => {
+        res.send('Add a book')
+    })
+    .put((req, res) => {
+        res.send('Update the book')
+    })
+
+
+app.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
 })
