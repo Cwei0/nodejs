5fc0d29eddd12e270080c1d069d5b62c16a26995
Cwei0 <bottgabriel@icloud.com>
2023-01-20 00:20:13 +0100

    peace

---
+++ router/api/employees.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router()
+const data = {}
+data.employees = require('../../data/employees.json')
+//App.route() fn used to chain route handlers
+// app.route('/book')
+//     .get((req, res) => {
+//         res.send('Get a random book')
+//     })
+//     .post((req, res) => {
+//         res.send('Add a book')
+//     })
+//     .put((req, res) => {
+//         res.send('Update the book')
+//     })
+
+router.route('/')
+    .get((req, res) => {
+        res.json(data.employees)
+    })
+    .post((req, res) => {
+        res.json({
+            "firstname": req.body.firstname,
+            "lastname": req.body.lastname,
+        })
+    })
+    .put((req, res) => {
+        res.json({
+            "firstname": req.body.firstname,
+            "lastname": req.body.lastname,
+        })
+    })
+    .delete((req, res) => {
+        res.json({ "id": req.body.id })
+    })
+router.route('/:id')
+    .get((req, res) => {
+        res.json({"id": req.params.id})
+    })
+
+module.exports = router
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { logEvents, logger } = require("./middleware/logEvent")
 const errorHandler = require("./middleware/errorHandler")
 
 const cors = require('cors')
-const PORT = process.env.PORT || 8500
+const PORT = process.env.PORT || 3500
 //Custom Middleware
 app.use(logger)
 
@@ -31,22 +31,15 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, '/public'))) //pushing css and img sheets
+app.use('/',express.static(path.join(__dirname, '/public')))
+app.use('/subdir' ,express.static(path.join(__dirname, '/public'))) //pushing css and img sheets
 
-app.get('^/$|/index(.html)?', (req, res) => {
-    // res.sendFile('./views/index.html', {root: __dirname})
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
+//Routes
+app.use('/', require('./router/root'))
+app.use('/subdir', require('./router/subdir'))
+app.use('/employees', require('./router/api/employees'))
 
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
-})
-
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, '/new-page.html')
-})
-
-app.all('*', (req, res) => {
+app.all('/*', (req, res) => {
     res.status(404);
     if(req.accepts('html')){
         res.sendFile(path.join(__dirname, 'views', '404.html'))
@@ -56,42 +49,6 @@ app.all('*', (req, res) => {
         res.type('txt').send("404 not found")
     }
 })
-
-//Route handlers
-//Example 1
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log('Attempted to load hello html page')
-    next()
-}, (req, res) => {
-    res.send('Hello World!')
-})
-//Example 2
-const cb0 = function (req, res, next) {
-    console.log('CB0')
-    next()
-}
-
-const cb1 = function (req, res, next) {
-    console.log('CB1')
-    next()
-}
-
-const cb2 = function (req, res) {
-    res.send('Hello from C!')
-}
-app.get('/example/c', [cb0, cb1, cb2])
-
-//App.route() fn used to chain route handlers
-// app.route('/book')
-//     .get((req, res) => {
-//         res.send('Get a random book')
-//     })
-//     .post((req, res) => {
-//         res.send('Add a book')
-//     })
-//     .put((req, res) => {
-//         res.send('Update the book')
-//     })
 
 app.use(errorHandler)
 app.listen(PORT, () => {
