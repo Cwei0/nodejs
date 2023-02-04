febeb58710533b11696ea5b9cc5ecf2567d90b81
Cwei0 <bottgabriel@icloud.com>
2023-02-04 21:34:37 +0100

    credentials update

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -2,8 +2,11 @@ const allowedOrigins = require('../config/allowedOrigins')
 
 const credentials = (req, res, next) => {
     const origin = req.headers.origin
-    if(allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Credentials', true)
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', allowedOrigins )
+        res.setHeader('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, PATCH, DELETE')
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        res.setHeader('Access-Control-Allow-Credentials', true)
     }
     next()
 }
